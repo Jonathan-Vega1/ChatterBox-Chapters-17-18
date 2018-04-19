e9bfffff95030338bd531244107cca5a095a24b9
Jonathan-Vega1 <Jonathan-Vega1@users.noreply.github.com>
2018-04-18 19:10:55 -0700

    Assignment 7
    This was about chapters 17 and 18. Chapter 18 was broken.

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true
+  },
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "sourceType": "module"
+  },
+  "extends": "eslint:recommended",
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "windows"
+    ],
+    "quotes": [
+      "error",
+      "double"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ]
+  }
+};
